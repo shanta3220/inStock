c2163ca4a112943e4ff72459bd5be0036d069161
Roshani Wickramasighe <roshaniwickramasighe@mac.home>
2024-11-13 09:55:47 -0500

    create route to the pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import WarehousesPage from "./pages/WarehousesPage/WarehousesPage";
+import InventoryPage from "./pages/InventoryPage/InventoryPage";
 
 function App() {
-  const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+    <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<WarehousesPage/>}/>
+    <Route path="/inventory" element={<InventoryPage/>}/>
+  </Routes>
+    </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
