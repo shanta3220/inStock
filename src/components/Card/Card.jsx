1ab0be3aa0f0da0fc7fcda3185402af73efaeba5
NJShanta <njshanta.ca@gmail.com>
2024-11-15 04:16:34 -0500

    Edit button added to card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,18 +1,41 @@
-import "./Card.scss"
+import "./Card.scss";
+import { useNavigate } from "react-router-dom";
+import backImage from "../../assets/Icons/arrow_back-24px.svg";
+import editIcon from "../../assets/Icons/edit-white-24px.svg";
 
+function Card({
+  title,
+  returnPath,
+  handleEditOnClick,
+  headerContent,
+  children,
+}) {
+  const navigate = useNavigate();
 
-function Card({ title, headerContent, children }) {
   return (
     <div className="card">
       <div className="card__header">
-      <h1 className="card__name">{title}</h1>
-      <div className="card__header-section">
-        {headerContent}
+        <div className="card__back-image-name-pair">
+          {returnPath && (
+            <img
+              className="card__back_image"
+              src={backImage}
+              onClick={() => {
+                navigate(returnPath);
+              }}
+            />
+          )}
+          <h1 className="card__name">{title}</h1>
+          {handleEditOnClick && (
+            <button
+              className="card__edit-button"
+              onClick={handleEditOnClick}
+            ></button>
+          )}
+        </div>
+        <div className="card__header-section">{headerContent}</div>
       </div>
-      </div>
-      <div className="card__content">
-        {children}
-      </div>
+      <div className="card__content">{children}</div>
     </div>
   );
 }
--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,18 +1,31 @@
-import Card from '../Card/Card'
+import "./WarehouseDetails.scss";
+import Card from "../Card/Card";
 
 function WarehouseDetails() {
+  const handleEditOnClick = () => {
+    console.log("Edit button clicked!");
+  };
+
   return (
-    <Card title ="Warehouses" headerContent={
+    <Card
+      title="Warehouses"
+      returnPath="/"
+      handleEditOnClick={handleEditOnClick}
+      headerContent={
         <div className="option">
           <div className="option__search-bar">
-            <input className="option__search-input" type="text" placeholder="Search..." />
-            <button className="option__search-button">
-            </button>
+            <input
+              className="option__search-input"
+              type="text"
+              placeholder="Search..."
+            />
+            <button className="option__search-button"></button>
           </div>
           <button className="option__add-button">+ Add New Warehouse</button>
         </div>
-      }></Card>
-    )
+      }
+    ></Card>
+  );
 }
 
 export default WarehouseDetails;
