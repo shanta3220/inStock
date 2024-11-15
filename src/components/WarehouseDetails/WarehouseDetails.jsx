a6eaa6e630457c6539bc0601e7d5e73beed03b61
NJShanta <njshanta.ca@gmail.com>
2024-11-15 04:54:43 -0500

    fetching warehouse details data

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,30 +1,19 @@
 import "./WarehouseDetails.scss";
 import Card from "../Card/Card";
 
-function WarehouseDetails() {
+function WarehouseDetails({ warehouseInfo }) {
   const handleEditOnClick = () => {
     console.log("Edit button clicked!");
   };
 
   return (
     <Card
-      title="Warehouses"
+      title={warehouseInfo.warehouse_name}
       returnPath="/"
       handleEditOnClick={handleEditOnClick}
-      headerContent={
-        <div className="option">
-          <div className="option__search-bar">
-            <input
-              className="option__search-input"
-              type="text"
-              placeholder="Search..."
-            />
-            <button className="option__search-button"></button>
-          </div>
-          <button className="option__add-button">+ Add New Warehouse</button>
-        </div>
-      }
-    ></Card>
+    >
+      <div></div>
+    </Card>
   );
 }
 
--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -1,13 +1,38 @@
-import "./WarehouseDetailsPage.scss"
+import "./WarehouseDetailsPage.scss";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
-import WarehouseList from "../../components/WarehouseList/WarehouseList";
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function WarehouseDetailsPage() {
+  const { id } = useParams();
+  const [warehouseInfo, setWarehouseInfo] = useState(null);
+  const [inventoryList, setInventoryList] = useState([]);
+
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_URL;
+
+    const getWarehouseDetails = async () => {
+      try {
+        const { data } = await axios.get(`${API_URL}/api/warehouses/${id}`);
+        setWarehouseInfo(data);
+      } catch (e) {
+        console.error("Error fetching warehouses:", e);
+      }
+    };
+
+    getWarehouseDetails();
+  }, [id]);
+
+  if (!warehouseInfo) {
+    return <dir>Loading...</dir>;
+  }
+
   return (
     <>
-    <WarehouseDetails/>
+      <WarehouseDetails warehouseInfo={warehouseInfo} />
     </>
-  )
+  );
 }
 
-export default WarehouseDetailsPage
+export default WarehouseDetailsPage;
