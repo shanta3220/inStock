9a90b4a8cfb6eda5dc4e0fc330b4f06813bf4260
NJShanta <njshanta.ca@gmail.com>
2024-11-15 11:23:58 -0500

    Fixes the page to used card component properly

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,45 +1,34 @@
 import "./WarehouseDetails.scss";
-import Card from "../Card/Card";
 
 function WarehouseDetails({ warehouse }) {
-  const handleEditOnClick = () => {
-    console.log("Edit button clicked!");
-  };
-
   return (
-    <Card
-      title={warehouse.warehouse_name}
-      returnPath="/"
-      handleEditOnClick={handleEditOnClick}
-    >
-      <div className="warehouse-info">
-        <div className="warehouse-info__cell-pair">
-          <h4 className="warehouse-info__title">WAREHOUSE ADDRESS:</h4>
-          <div className="warehouse-info__content" id="address">
-            <div className="warehouse-info__address">
-              <p>{warehouse.address},&nbsp;</p>
-              <p>
-                {warehouse.city},&nbsp;{warehouse.country}
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="warehouse-info__cell-pair">
-          <h4 className="warehouse-info__title">CONTACT NAME:</h4>
-          <div className="warehouse-info__content">
-            <p>{warehouse.contact_name}</p>
-            <p>{warehouse.contact_position}</p>
-          </div>
-        </div>
-        <div className="warehouse-info__cell-pair">
-          <h4 className="warehouse-info__title">CONTACT INFORMATION:</h4>
-          <div className="warehouse-info__content">
-            <p>{warehouse.contact_phone}</p>
-            <p>{warehouse.contact_email}</p>
+    <div className="warehouse-info">
+      <div className="warehouse-info__cell-pair">
+        <h4 className="warehouse-info__title">WAREHOUSE ADDRESS:</h4>
+        <div className="warehouse-info__content" id="address">
+          <div className="warehouse-info__address">
+            <p>{warehouse.address},&nbsp;</p>
+            <p>
+              {warehouse.city},&nbsp;{warehouse.country}
+            </p>
           </div>
         </div>
       </div>
-    </Card>
+      <div className="warehouse-info__cell-pair">
+        <h4 className="warehouse-info__title">CONTACT NAME:</h4>
+        <div className="warehouse-info__content">
+          <p>{warehouse.contact_name}</p>
+          <p>{warehouse.contact_position}</p>
+        </div>
+      </div>
+      <div className="warehouse-info__cell-pair">
+        <h4 className="warehouse-info__title">CONTACT INFORMATION:</h4>
+        <div className="warehouse-info__content">
+          <p>{warehouse.contact_phone}</p>
+          <p>{warehouse.contact_email}</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
--- src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
+++ src/pages/WarehouseDetailsPage/WarehouseDetailsPage.jsx
@@ -1,13 +1,18 @@
 import "./WarehouseDetailsPage.scss";
 import WarehouseDetails from "../../components/WarehouseDetails/WarehouseDetails";
+import Card from "../../components/Card/Card";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
 function WarehouseDetailsPage() {
   const { id } = useParams();
-  const [warehouseInfo, setWarehouseInfo] = useState(null);
+  const [warehouse, setWarehouse] = useState(null);
   const [inventoryList, setInventoryList] = useState([]);
+
+  const handleEditOnClick = () => {
+    console.log("Edit button clicked!");
+  };
 
   useEffect(() => {
     const API_URL = import.meta.env.VITE_API_URL;
@@ -15,7 +20,7 @@ function WarehouseDetailsPage() {
     const getWarehouseDetails = async () => {
       try {
         const { data } = await axios.get(`${API_URL}/api/warehouses/${id}`);
-        setWarehouseInfo(data);
+        setWarehouse(data);
       } catch (e) {
         console.error("Error fetching warehouses:", e);
       }
@@ -24,13 +29,19 @@ function WarehouseDetailsPage() {
     getWarehouseDetails();
   }, [id]);
 
-  if (!warehouseInfo) {
+  if (!warehouse) {
     return <dir>Loading...</dir>;
   }
 
   return (
     <>
-      <WarehouseDetails warehouse={warehouseInfo} />
+      <Card
+        title={warehouse.warehouse_name}
+        returnPath="/"
+        handleEditOnClick={handleEditOnClick}
+      >
+        <WarehouseDetails warehouse={warehouse} />
+      </Card>
     </>
   );
 }
