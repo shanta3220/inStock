d3d41340432d2806e3c671587d4b308a0a1628ff
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-15 14:24:43 -0500

    add editInventoryPage

---
+++ src/pages/EditInventoryPage/EditInventoryPage.jsx
@@ -0,0 +1,15 @@
+import Card from "../../components/Card/Card"
+
+
+function EditInventoryPage() {
+  return (
+    <Card title= "Edit Inventory Item" returnPath="/inventory">
+      
+   
+    
+
+    </Card>
+  )
+}
+
+export default EditInventoryPage
