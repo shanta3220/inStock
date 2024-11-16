9713f6b0f0360d47ab80897a5b2e1f3777f594cd
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-16 01:39:04 -0500

    add button styling

--- src/components/CancelSaveButtons/CancelSaveButtons.jsx
+++ src/components/CancelSaveButtons/CancelSaveButtons.jsx
@@ -2,9 +2,9 @@ import './CancelSaveButtons.scss'
 
 function CancelSaveButtons() {
   return (
-    <div className='buttons-bar'>
-      <button>Cancel</button>
-      <button>Save</button>
+    <div className='buttons'>
+      <button className='buttons__button'>Cancel</button>
+      <button className='buttons__button buttons__button--save'>Save</button>
     </div>
   )
 }
