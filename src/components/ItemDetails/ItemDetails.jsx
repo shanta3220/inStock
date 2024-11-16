5acebb756b03708b14109ced8dfe936a911aa3be
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-15 23:59:10 -0500

    add the basic item detail component with the styling for mobile version

--- src/components/ItemDetails/ItemDetails.jsx
+++ src/components/ItemDetails/ItemDetails.jsx
@@ -1,29 +1,49 @@
-import React from 'react'
+import "./ItemDetails.scss";
+import arrowDown from "../../assets/Icons/arrow_drop_down-24px.svg"
 
 const ItemDetails = () => {
   return (
-    <div>
-      <h2>Item details</h2>
-      <label>
-        Item name
-        <input placeholder='Television'/> 
-        {/* update the placeholder with the item name clicked */}
+    <div className="item-details">
+      <h2 className="item-details__header">Item details</h2>
+      <label id="name" name="name" className="item-details__label">
+        Item Name
       </label>
-      <label>
+      <input
+        id="name"
+        name="name"
+        placeholder="Television"
+        className="item-details__input"
+      />
+      {/* update the placeholder with the item name clicked */}
+      <label id="details" name="details" className="item-details__label">
         Description
-        <input placeholder='This 50", 4K LED TV provides a crystal-clear picture and vivid colors.'/>
-        {/* update the placeholder with the item description */}
       </label>
-      <label>
-        Category
-        <select>
-        <option value="" disabled>
-          Select an option
-        </option>
-        </select>
-      </label>
+      <textarea
+        id="details"
+        name="details"
+        rows="4" 
+        maxLength="500"
+        className="item-details__textarea"
+        placeholder='This 50", 4K LED TV provides a crystal-clear picture and vivid colors.'
+      />
+      {/* update the placeholder with the item description */}
+      <div className="item-details__dropdown-container">
+        <label className="item-details__label">Category</label>
+        <div className="item-details__input-wrapper">
+          <input
+            className="item-details__input"
+            type="text"
+            placeholder="Select Category"
+          />
+          <img
+            src={arrowDown}
+            alt="Dropdown"
+            className="item-details__dropdown-icon"
+          />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ItemDetails
+export default ItemDetails;
