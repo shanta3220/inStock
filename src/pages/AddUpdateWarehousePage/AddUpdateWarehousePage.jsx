a4e44aaab75b132ef8440e54c368630e5321419a
NJShanta <njshanta.ca@gmail.com>
2024-11-16 18:31:36 -0500

    Add the form inputs

--- src/pages/AddUpdateWarehousePage/AddUpdateWarehousePage.jsx
+++ src/pages/AddUpdateWarehousePage/AddUpdateWarehousePage.jsx
@@ -10,13 +10,27 @@ function AddUpdateWarehousePage() {
   const warehouse = null;
 
   useEffect(() => {}, []);
-  const [warehouseName, setWarehouseName] = useState("");
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [contactName, setContactName] = useState("");
+  const [position, setPosition] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
 
-  const handleWarehouseNameChange = () => {};
+  const handleNameChange = () => {};
+  const handleAddressChange = () => {};
+  const handleCityChange = () => {};
+  const handleCountryChange = () => {};
+  const handleContactNameChange = () => {};
+  const handlePositionChange = () => {};
+  const handlePhoneNumberChange = () => {};
+  const handleEmailChange = () => {};
 
   return (
     <>
@@ -29,15 +43,102 @@ function AddUpdateWarehousePage() {
             <h2 className="warehouse-form__category-title">
               Warehouse Details
             </h2>
-            <label className="upload-form__label">
+            <label className="warehouse-form__label">
               Warehouse Name
               <input
                 type="text"
-                name="warehouseName"
-                value={warehouseName}
+                name="name"
+                value={name}
                 placeholder="Warehouse Name"
                 className="warehouse-form__input-field"
-                onChange={handleWarehouseNameChange}
+                onChange={handleNameChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              Street Address
+              <input
+                type="text"
+                name="address"
+                value={address}
+                placeholder="Street Address"
+                className="warehouse-form__input-field"
+                onChange={handleAddressChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              City
+              <input
+                type="text"
+                name="city"
+                value={city}
+                placeholder="City"
+                className="warehouse-form__input-field"
+                onChange={handleCityChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              Country
+              <input
+                type="text"
+                name="country"
+                value={country}
+                placeholder="Country"
+                className="warehouse-form__input-field"
+                onChange={handleCountryChange}
+                required
+              />
+            </label>
+          </div>
+          <div className="warehouse-form__category">
+            <h2 className="warehouse-form__category-title">Contact Details</h2>
+            <label className="warehouse-form__label">
+              Contact Name
+              <input
+                type="text"
+                name="contactName"
+                value={contactName}
+                placeholder="Contact Name"
+                className="warehouse-form__input-field"
+                onChange={handleContactNameChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              Position
+              <input
+                type="text"
+                name="position"
+                value={position}
+                placeholder="Position"
+                className="warehouse-form__input-field"
+                onChange={handlePositionChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              Phone Number
+              <input
+                type="text"
+                name="phoneNumber"
+                value={phoneNumber}
+                placeholder="Phone Number"
+                className="warehouse-form__input-field"
+                onChange={handlePhoneNumberChange}
+                required
+              />
+            </label>
+            <label className="warehouse-form__label">
+              Email
+              <input
+                type="text"
+                name="email"
+                value={email}
+                placeholder="Email"
+                className="warehouse-form__input-field"
+                onChange={handleEmailChange}
                 required
               />
             </label>
