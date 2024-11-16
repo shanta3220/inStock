04a8cec76039808f5f6dd3e1d2df61909f5a6d24
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-16 02:20:53 -0500

    add the stylying to tablet view

--- src/pages/EditInventoryPage/EditInventoryPage.jsx
+++ src/pages/EditInventoryPage/EditInventoryPage.jsx
@@ -2,15 +2,16 @@ import CancelSaveButtons from "../../components/CancelSaveButtons/CancelSaveButt
 import Card from "../../components/Card/Card"
 import ItemAvailability from "../../components/ItemAvailability/ItemAvailability"
 import ItemDetails from "../../components/ItemDetails/ItemDetails"
+import './EditInventoryPage.scss'
 
 
 function EditInventoryPage() {
   return (
     <Card title= "Edit Inventory Item" returnPath="/inventory">
-
-   
+      <div className="form-fields">
     <ItemDetails/>
     <ItemAvailability/>
+    </div>
 <CancelSaveButtons/>
     </Card>
   )
