11f57e7e99d65e67fa67bef541dda12f77361f89
Roshani Faleel <roshfaleel98@gmail.com>
2024-11-16 21:42:16 -0500

    removed the edit inventory page from inventory page which use in testing purpose

--- src/pages/InventoryPage/InventoryPage.jsx
+++ src/pages/InventoryPage/InventoryPage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
-import EditInventoryPage from '../EditInventoryPage/EditInventoryPage'
 
 function InventoryPage() {
   return (
     <div>
       Inventory Page
-      <EditInventoryPage/>
     </div>
   )
 }
